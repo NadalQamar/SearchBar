87e8326674272c0c9b6c586af13748d05fc89ad8
Nadal Qamar <qamar_173na@outlook.com>
2022-08-18 06:35:17 +0500

    Add project files.

---
+++ SearchBar.Designer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace SearchBar
+{
+    partial class SearchBar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchLabel = new System.Windows.Forms.Label();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // searchLabel
+            // 
+            this.searchLabel.AutoSize = true;
+            this.searchLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchLabel.Location = new System.Drawing.Point(12, 9);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Size = new System.Drawing.Size(387, 25);
+            this.searchLabel.TabIndex = 2;
+            this.searchLabel.Text = "Enter your query and then press enter: ";
+            // 
+            // searchBox
+            // 
+            this.searchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBox.Location = new System.Drawing.Point(34, 41);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(427, 26);
+            this.searchBox.TabIndex = 1;
+            this.searchBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.searchBox_PressEnter);
+            // 
+            // SearchBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(481, 80);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.searchLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SearchBar";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.SearchBar_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.TextBox searchBox;
+    }
+}
---
+++ SearchBar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SearchBar
+{
+   
+    public partial class SearchBar : Form
+    {
+        string searchText;
+        public SearchBar()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBar_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void searchBox_PressEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                searchText = searchBox.Text;
+                try
+                {
+                    Process.Start($"https://www.google.com/search?q={searchText}");
+                }
+                catch (Win32Exception noBrowser)
+                {
+                    if (noBrowser.ErrorCode == -2137367259)
+                    {
+                        MessageBox.Show(noBrowser.Message);
+                    }
+                }
+                catch (System.Exception other)
+                {
+                    MessageBox.Show(other.Message);
+                }
+                
+            }
+        }
+    }
+}
