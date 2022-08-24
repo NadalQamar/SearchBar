39ada30185d4b0b43cee7060e1a614fd6a993bed
Nadal Qamar <qamar_173na@outlook.com>
2022-08-24 23:17:06 +0500

    Need to add the branch to github for later updating the master

--- SearchBar.Designer.cs
+++ SearchBar.Designer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace SearchBar
 {
@@ -30,29 +32,28 @@ namespace SearchBar
         /// </summary>
         private void InitializeComponent()
         {
-            this.searchLabel = new System.Windows.Forms.Label();
-            this.searchBox = new System.Windows.Forms.TextBox();
-            this.searchEngineSelector = new System.Windows.Forms.ComboBox();
+            this.searchLabel = new Label();
+            this.searchBox = new TextBox();
+            this.searchEngineSelector = new ComboBox();
             this.SuspendLayout();
             // 
             // searchLabel
             // 
-            this.searchLabel.AutoSize = true;
-            this.searchLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.searchLabel.Location = new System.Drawing.Point(12, 9);
+            this.searchLabel.Font = new Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchLabel.Location = new Point(2, 2);
             this.searchLabel.Name = "searchLabel";
-            this.searchLabel.Size = new System.Drawing.Size(387, 25);
+            this.searchLabel.Size = new Size(387, 25);
             this.searchLabel.TabIndex = 3;
             this.searchLabel.Text = "Enter your query and then press enter: ";
             // 
             // searchBox
             // 
-            this.searchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.searchBox.Location = new System.Drawing.Point(34, 41);
+            this.searchBox.Font = new Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBox.Location = new Point(20, 32);
             this.searchBox.Name = "searchBox";
-            this.searchBox.Size = new System.Drawing.Size(427, 26);
+            this.searchBox.Size = new Size(427, 26);
             this.searchBox.TabIndex = 1;
-            this.searchBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.searchBox_PressEnter);
+            this.searchBox.KeyDown += new KeyEventHandler(this.SearchBox_PressEnter);
             // 
             // searchEngineSelector
             // 
@@ -60,38 +61,36 @@ namespace SearchBar
             this.searchEngineSelector.Items.AddRange(new object[] {
             "Google",
             "Bing"});
-            this.searchEngineSelector.Location = new System.Drawing.Point(34, 74);
+            this.searchEngineSelector.Location = new Point(20, 60);
             this.searchEngineSelector.Name = "searchEngineSelector";
-            this.searchEngineSelector.Size = new System.Drawing.Size(137, 21);
+            this.searchEngineSelector.Size = new Size(137, 21);
             this.searchEngineSelector.TabIndex = 2;
             // 
             // SearchBar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(481, 116);
+            this.ClientSize = new Size(454, 91);
             this.Controls.Add(this.searchEngineSelector);
             this.Controls.Add(this.searchBox);
             this.Controls.Add(this.searchLabel);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SearchBar";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
-            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.SizeGripStyle = SizeGripStyle.Hide;
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Width - 504, Screen.PrimaryScreen.Bounds.Height - 141);
             this.TopMost = true;
-            this.Load += new System.EventHandler(this.SearchBar_Load);
+            this.Load += new EventHandler(this.SearchBar_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label searchLabel;
-        private System.Windows.Forms.TextBox searchBox;
-        private System.Windows.Forms.ComboBox searchEngineSelector;
+        private Label searchLabel;
+        private TextBox searchBox;
+        private ComboBox searchEngineSelector;
     }
 }
--- SearchBar.cs
+++ SearchBar.cs
@@ -25,16 +25,125 @@ namespace SearchBar
 
         private void SearchBar_Load(object sender, EventArgs e)
         {
+            
             searchEngineSelector.SelectedIndex = 0;
+            searchBox.Focus();
 
         }
+        //Methods
+        private string BeginningOfString(string str)
+        {
+            char c = str[0];
+            int iteration = 0;
 
-        private void searchBox_PressEnter(object sender, KeyEventArgs e)
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == c)//for whichever character that starts the string
+                {
+                    iteration++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            for (int i = 0; i < iteration; i++)//remove the number of repeated character of same type
+            {
+                str = str.Substring(1);
+            }
+            for (int i = 0; i < iteration; i++)//add the same number of character of same type
+            {
+                if(c == ' ')
+                {
+                    break;
+                }
+                if(c == '+')
+                {
+                    str = "%2B" + str;
+                }
+                if (c == '=')
+                {
+                    str = "%3D" + str;
+                }
+                if (c == '/')
+                {
+                    str = "%2F" + str;
+                }
+
+            }
+            return str;
+        }
+        private string MiddleOfString(char c)
+        {
+            string str = "";
+            if(c == '+')
+            {
+                str = "%2B";
+            }
+            if (c == '=')
+            {
+                str = "%3D";
+            }
+            if (c == '/')
+            {
+                str = "%2F";
+            }
+            return str;
+        }
+
+        private void SearchBox_PressEnter(object sender, KeyEventArgs e)
         {
         //Key Feature
             if (e.KeyCode == Keys.Enter)
             {
                 searchText = searchBox.Text;
+                searchText = BeginningOfString(searchText);
+                //if search query is spaced
+                if (searchText.Contains (" "))
+                {
+
+                    string[] splitString = searchText.Split(' ');
+                    searchText = "";
+                    for (int i = 0; i < splitString.Length; i++)
+                    {
+                        string insertCharacter = "";
+                        if (i == 0)
+                        {
+
+                            for(int j = 0; j < splitString[i].Length; j++)
+                            {
+                                char[] chars = splitString[i].ToCharArray();
+                                if (chars[j] == '+' || chars[j] == '=' || chars[j] == '/')
+                                {
+                                    insertCharacter = MiddleOfString(chars[j]);
+                                    splitString[i] = splitString[i].Insert(j, insertCharacter);
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                            searchText = splitString[i];
+                        }
+                        else
+                        {   //if search query has any un-needed spaces
+                            if (splitString[i] == "")
+                            {
+                                continue;
+                            }
+                            char mainC = searchText[i];
+                            if (mainC == '+' || mainC == '=' || mainC == '/')//good reference to use ASCII maybe?
+                            {
+                                insertCharacter = MiddleOfString(mainC);
+                                searchText = searchText.Insert(i, insertCharacter);
+                            }
+                            else
+                            {
+                                searchText = searchText + "+" + splitString[i];
+                            }
+                        }
+                    }
+                }
                 try
                 {//SubFeature
                     if(searchEngineSelector.SelectedIndex == 0)
